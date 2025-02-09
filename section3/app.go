d7637f583e500501d2be703623fe088dcca7de39
Pool Golez <loop.edward@gmail.com>
2025-02-09 10:10:22 +0800

    Section 3: Refactor business logice from main function

--- section3/app.go
+++ section3/app.go
@@ -4,8 +4,26 @@ import "fmt"
 
 func main() {
 	accountBalance := 1200.0
-	var choice int8;
+	
+	choice := promptUserAction()
+	switch(choice) {
+	case 1:
+		fmt.Printf("Your balance is %.2f\n", accountBalance)
+	case 2:
+		accountBalance = withdrawAction(accountBalance)
+		fmt.Printf("Your remaining balance is %.2f\n", accountBalance)
+	case 3:
+		accountBalance = depositAction(accountBalance)
+		fmt.Printf("Your updated balance is %.2f\n", accountBalance)
+	default:
+		fmt.Println("Exiting now.")
+	}
 
+	fmt.Println("Thank you for banking with us!")
+}
+
+func promptUserAction() int8 {
+	var choice int8;
 	welcomeText:=`Welcome to the bank! How can I help you?
 	1. Check Balance
 	2. Withdraw amount
@@ -16,23 +34,21 @@ func main() {
 	fmt.Print("Choice: ")
 	fmt.Scan(&choice)
 
-	switch(choice) {
-	case 1:
-		fmt.Printf("Your balance is %.2f\n", accountBalance)
-	case 2:
-		var amount float64
-		fmt.Print("Enter amount to withdraw:")
-		fmt.Scan(&amount)
-		accountBalance -= amount
-		fmt.Printf("Your remaining balance is %.2f\n", accountBalance)
-	case 3:
-		var amount float64;
-		fmt.Print("Enter amount to deposit:")
-		fmt.Scan(&amount)
-		accountBalance += amount
-		fmt.Printf("Your updated balance is %.2f\n", accountBalance)
-	default:
-		fmt.Println("Exiting now.")
-	}
+	return choice
+}
 
+func withdrawAction(accountBalance float64) float64 {
+	var amount float64
+	fmt.Print("Enter amount to withdraw:")
+	fmt.Scan(&amount)
+
+	return accountBalance - amount
+}
+
+func depositAction(accountBalance float64) float64 {
+	var amount float64;
+	fmt.Print("Enter amount to deposit:")
+	fmt.Scan(&amount)
+	
+	return accountBalance + amount
 }
