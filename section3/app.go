c51c9aaba27d720cf3847df62bde6dbb8f15307e
Pool Golez <loop.edward@gmail.com>
2025-02-09 10:05:16 +0800

    Section 3: Implement bank options

--- section3/app.go
+++ section3/app.go
@@ -3,5 +3,36 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Inside Bank account sample")
+	accountBalance := 1200.0
+	var choice int8;
+
+	welcomeText:=`Welcome to the bank! How can I help you?
+	1. Check Balance
+	2. Withdraw amount
+	3. Deposit amount
+	4. Exit`
+	fmt.Println(welcomeText);
+	
+	fmt.Print("Choice: ")
+	fmt.Scan(&choice)
+
+	switch(choice) {
+	case 1:
+		fmt.Printf("Your balance is %.2f\n", accountBalance)
+	case 2:
+		var amount float64
+		fmt.Print("Enter amount to withdraw:")
+		fmt.Scan(&amount)
+		accountBalance -= amount
+		fmt.Printf("Your remaining balance is %.2f\n", accountBalance)
+	case 3:
+		var amount float64;
+		fmt.Print("Enter amount to deposit:")
+		fmt.Scan(&amount)
+		accountBalance += amount
+		fmt.Printf("Your updated balance is %.2f\n", accountBalance)
+	default:
+		fmt.Println("Exiting now.")
+	}
+
 }
