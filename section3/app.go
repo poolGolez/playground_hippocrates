218d7a2387221713f7eedce2b2f5afc4fbd157a1
Pool Golez <loop.edward@gmail.com>
2025-02-09 10:19:48 +0800

    Section 4: Implement looping until exit

--- section3/app.go
+++ section3/app.go
@@ -4,35 +4,51 @@ import "fmt"
 
 func main() {
 	accountBalance := 1200.0
-	
-	choice := promptUserAction()
-	switch(choice) {
-	case 1:
-		fmt.Printf("Your balance is %.2f\n", accountBalance)
-	case 2:
-		accountBalance = withdrawAction(accountBalance)
-		fmt.Printf("Your remaining balance is %.2f\n", accountBalance)
-	case 3:
-		accountBalance = depositAction(accountBalance)
-		fmt.Printf("Your updated balance is %.2f\n", accountBalance)
-	default:
-		fmt.Println("Exiting now.")
-	}
 
-	fmt.Println("Thank you for banking with us!")
+	for {
+		choice := promptUserAction()
+		switch choice {
+		case 1:
+			fmt.Println("=============================================")
+			fmt.Printf("Your balance is %.2f\n", accountBalance)
+			fmt.Println("=============================================")
+		case 2:
+			accountBalance = withdrawAction(accountBalance)
+			fmt.Println("=============================================")
+
+			fmt.Printf("Your remaining balance is %.2f\n", accountBalance)
+			fmt.Println("=============================================")
+		case 3:
+			accountBalance = depositAction(accountBalance)
+			fmt.Println("=============================================")
+			fmt.Printf("Your updated balance is %.2f\n", accountBalance)
+			fmt.Println("=============================================")
+		default:
+			fmt.Println("=============================================")
+			fmt.Println("Thank you for banking with us!")
+			fmt.Println("=============================================")
+			return
+		}
+	}
 }
 
-func promptUserAction() int8 {
-	var choice int8;
-	welcomeText:=`Welcome to the bank! How can I help you?
+func promptUserAction() int {
+	var choice int
+	welcomeText := `Welcome to the bank! How can I help you?
 	1. Check Balance
 	2. Withdraw amount
 	3. Deposit amount
 	4. Exit`
-	fmt.Println(welcomeText);
-	
-	fmt.Print("Choice: ")
-	fmt.Scan(&choice)
+	fmt.Println(welcomeText)
+
+	for choice == 0 || choice > 4 {
+		fmt.Print("Choice: ")
+		fmt.Scan(&choice)
+
+		if choice > 4 {
+			fmt.Println("Invalid choice; please choose again")
+		}
+	}
 
 	return choice
 }
@@ -46,9 +62,9 @@ func withdrawAction(accountBalance float64) float64 {
 }
 
 func depositAction(accountBalance float64) float64 {
-	var amount float64;
+	var amount float64
 	fmt.Print("Enter amount to deposit:")
 	fmt.Scan(&amount)
-	
+
 	return accountBalance + amount
 }
