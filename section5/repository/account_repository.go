8c0f3bca345aeae524582880d5b3553e5bc85090
Pool Golez <loop.edward@gmail.com>
2025-02-18 20:17:57 +0800

    Section 5: Implement CSV as database

--- section5/repository/account_repository.go
+++ section5/repository/account_repository.go
@@ -2,6 +2,8 @@ package repository
 
 import (
 	"encoding/csv"
+	"fmt"
+	"log"
 	"os"
 	"strconv"
 
@@ -11,6 +13,37 @@ import (
 const ACCOUNTS_TABLE = "./resources/accounts-table.csv"
 
 func Find(id string) *domain.Account {
+	records := getCsvRows()
+
+	var targetRow []string = nil
+	for _, record := range records {
+		if record[0] == id {
+			targetRow = record
+		}
+	}
+
+	if targetRow == nil {
+		return nil
+	}
+
+	return toAccount(targetRow)
+}
+
+func Update(account domain.Account) domain.Account {
+	records := getCsvRows()
+
+	for _, record := range records {
+		if record[0] == account.Id {
+			record[1] = strconv.FormatFloat(account.Balance, 'f', 2, 64)
+		}
+	}
+
+	writeCsv(records)
+
+	return account
+}
+
+func getCsvRows() [][]string {
 	file, err := os.Open(ACCOUNTS_TABLE)
 	if err != nil {
 		panic("Error opening the file")
@@ -23,25 +56,36 @@ func Find(id string) *domain.Account {
 		panic("Error reading from file")
 	}
 
-	var targetRow []string = nil
-	for i, record := range records {
-		if record[0] == id {
-			targetRow = record
-		}
+	return records[1:]
+}
+
+func writeCsv(rows [][]string) {
+	file, err := os.OpenFile(ACCOUNTS_TABLE, os.O_WRONLY, os.ModeAppend)
+	if err != nil {
+		panic("Error opening the file")
 	}
+	defer file.Close()
 
-	if targetRow == nil {
-		return nil
-	}
+	headers := [2]string{"ID", "Balance"}
 
-	balance, _ := strconv.ParseFloat(targetRow[1], 64)
+	writer := csv.NewWriter(file)
+	records := [][]string{}
+	records = append(records, headers[:])
+	records = append(records, rows...)
 
-	return &domain.Account{
-		Id:      targetRow[0],
-		Balance: balance,
+	fmt.Println(records)
+	writer.WriteAll(records)
+
+	if err := writer.Error(); err != nil {
+		log.Fatalln("Error saving records in file", err)
+		panic("Error saving records in file")
 	}
 }
 
-func Update(account domain.Account) domain.Account {
-	return account
+func toAccount(row []string) *domain.Account {
+	balance, _ := strconv.ParseFloat(row[1], 64)
+	return &domain.Account{
+		Id:      row[0],
+		Balance: balance,
+	}
 }
