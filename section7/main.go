4b2d741230454b3aaca1d96c8b0183a5d8a34eba
Pool Golez <loop.edward@gmail.com>
2025-02-19 17:46:50 +0800

    Section 7: Accept functions as parameters

--- section7/main.go
+++ section7/main.go
@@ -2,6 +2,32 @@ package main
 
 import "fmt"
 
+type intTransformFn = func(int) int
+
 func main() {
-	fmt.Println("Hello for Functional Programming")
+	numbers := []int{2, 4, 3, 9, 5, -2, 7}
+	doubled := *transformNumbers(&numbers, double)
+	squared := *transformNumbers(&numbers, square)
+
+	fmt.Println("Numbers ", numbers)
+	fmt.Println("Doubled ", doubled)
+	fmt.Println("Squared ", squared)
+}
+
+func transformNumbers(numbers *[]int, transform intTransformFn) *[]int {
+	result := make([]int, len(*numbers))
+
+	for i, n := range *numbers {
+		result[i] = transform(n)
+	}
+
+	return &result
+}
+
+func double(n int) int {
+	return n * 2
+}
+
+func square(n int) int {
+	return n * n
 }
