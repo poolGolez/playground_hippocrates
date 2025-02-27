e4dcbd09af1b0cfc449c82e5e7c5b600cd00e272
Pool Golez <loop.edward@gmail.com>
2025-02-27 18:06:49 +0800

    Section 9: Create tasks and execute sequentially

--- section9/main.go
+++ section9/main.go
@@ -1,7 +1,42 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+type Task func()
 
 func main() {
-	fmt.Println("Inside Section 9")
+	tasks := *createTasks(5)
+
+	durationStart := time.Now()
+	for _, task := range tasks {
+		task()
+	}
+	durationEnd := time.Now()
+
+	duration := durationEnd.Sub(durationStart)
+	fmt.Printf("Total Process Time: %.2f second(s)\n", float64(duration.Milliseconds())/1000.0)
+	fmt.Printf("Total Process Time: %.2f second(s)\n", duration.Seconds())
+}
+
+func createTasks(count int) *[]Task {
+	tasks := make([]Task, count)
+	for i := range tasks {
+		name := fmt.Sprintf("Task %d", i)
+		tasks[i] = createTask(name)
+	}
+	return &tasks
+}
+
+func createTask(name string) func() {
+	duration := time.Duration(rand.Intn(4)+1) * time.Second
+
+	return func() {
+		fmt.Printf("[%s] Executing task in %7.2f second(s)...\n", name, duration.Seconds())
+		time.Sleep(duration)
+		fmt.Printf("[%s] Task successfully executed...\n", name)
+	}
 }
