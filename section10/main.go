913d8ea99813b85abef548661f36aeea97ad615a
Pool Golez <loop.edward@gmail.com>
2025-02-27 20:12:13 +0800

    Section 10: Implement hello world endpoint

--- section10/main.go
+++ section10/main.go
@@ -1,7 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
 	fmt.Println("Creating Gin Server for Loans...")
+	server := gin.Default()
+	server.SetTrustedProxies(nil)
+
+	server.GET("/", helloWorld)
+
+	server.Run()
+}
+
+func helloWorld(ctx *gin.Context) {
+	ctx.JSON(http.StatusOK, gin.H{"message": "Hello world"})
 }
