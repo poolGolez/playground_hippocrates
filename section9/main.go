9f645426c479b19b6726711a677664499d594ac7
Pool Golez <loop.edward@gmail.com>
2025-02-27 18:47:20 +0800

    Section 9: Error handling for tasks

--- section9/main.go
+++ section9/main.go
@@ -9,45 +9,56 @@ import (
 type Task func()
 
 func main() {
-	tasksRef, channelsRef := createTasks(5)
-	tasks, channels := *tasksRef, *channelsRef
+	tasksRef, doneChannelsRef, errChannelsRef := createTasks(9)
+	tasks, doneChannels, errChannels := *tasksRef, *doneChannelsRef, *errChannelsRef
 
 	durationStart := time.Now()
 	for _, task := range tasks {
 		go task()
 	}
 
-	for _, channel := range channels {
-		<-channel
+	for index := range tasks {
+		select {
+		case name := <-doneChannels[index]:
+			fmt.Printf("[%s] Task successfully executed...\n", name)
+		case err := <-(errChannels[index]):
+			fmt.Println("An error happened:", err)
+		}
 	}
 
 	durationEnd := time.Now()
 
 	duration := durationEnd.Sub(durationStart)
-	fmt.Printf("Total Process Time: %.2f second(s)\n", float64(duration.Milliseconds())/1000.0)
 	fmt.Printf("Total Process Time: %.2f second(s)\n", duration.Seconds())
 }
 
-func createTasks(count int) (*[]Task, *[]chan bool) {
+func createTasks(count int) (*[]Task, *[]chan any, *[]chan error) {
 	tasks := make([]Task, count)
-	channels := make([]chan bool, count)
+	doneChannels := make([]chan any, count)
+	errorChannels := make([]chan error, count)
 
 	for i := range tasks {
-		name := fmt.Sprintf("Task %d", i)
-		channel := make(chan bool)
+		name := fmt.Sprintf("Task %d", i+1)
+		done := make(chan any)
+		err := make(chan error)
 
-		channels[i] = channel
-		tasks[i] = createTask(name, channel)
+		doneChannels[i] = done
+		errorChannels[i] = err
+		tasks[i] = createTask(name, done, err)
 	}
-	return &tasks, &channels
+	return &tasks, &doneChannels, &errorChannels
 }
 
-func createTask(name string, channel chan bool) Task {
-	duration := time.Duration(rand.Intn(5)+1) * time.Second
+func createTask(name string, doneChannel chan any, errChannel chan error) Task {
+	duration := time.Duration(rand.Intn(7)+1) * time.Second
 	return func() {
 		fmt.Printf("[%s] Executing task in %7.2f second(s)...\n", name, duration.Seconds())
 		time.Sleep(duration)
-		channel <- true
-		fmt.Printf("[%s] Task successfully executed...\n", name)
+
+		if duration <= time.Duration(4)*time.Second {
+			doneChannel <- name
+		} else {
+			errChannel <- fmt.Errorf("[%s] timed out for %.2f seconds", name, duration.Seconds())
+		}
 	}
 }
