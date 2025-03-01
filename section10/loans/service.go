efe2f21f5c4605f89b5f804404393f4306dd56a5
Pool Golez <loop.edward@gmail.com>
2025-03-01 09:59:07 +0800

    Section 10: Cleanup in-memory loans

--- section10/loans/service.go
+++ section10/loans/service.go
@@ -1,24 +1,10 @@
 package loans
 
-var loans []Loan = []Loan{
-	{
-		Principal:          2500000.0,
-		AnnualInterestRate: 0.0675,
-		YearsPayable:       10,
-	},
-	{
-		Principal:          2500000.0,
-		AnnualInterestRate: 0.0675,
-		YearsPayable:       15,
-	},
-}
-
 func FetchAll() []Loan {
 	return findAll()
 }
 
 func Save(loan *Loan) *Loan {
-	loans = append(loans, *loan)
 	save(loan)
 	return loan
 }
